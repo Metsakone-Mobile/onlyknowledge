8aa2a840bfe5a835fe2b99f184644ff27b7ec951
JaakkoMal <j.j.malmi@gmail.com>
2022-11-08 17:28:53 +0200

    Update Config.js

--- firebase/Config.js
+++ firebase/Config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
 
-
+// Add firebase configurations here
 
   initializeApp(firebaseConfig)
 
