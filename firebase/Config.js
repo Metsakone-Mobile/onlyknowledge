67415bd3481947da03f81ef0a2ec953a2a63e1d5
jmetsaniemi <91074405+jmetsaniemi@users.noreply.github.com>
2022-11-14 19:13:02 +0200

    Update Config.js

--- firebase/Config.js
+++ firebase/Config.js
@@ -4,17 +4,7 @@ import { getFirestore, collection, doc, addDoc, getDoc, setDoc, query, onSnapsho
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyCNxbPczkUpQM2mUTu0Kno7eyo48qNv6-c",
-
-  authDomain: "onlyknowledge-e6966.firebaseapp.com",
-
-  projectId: "onlyknowledge-e6966",
-
-  storageBucket: "onlyknowledge-e6966.appspot.com",
-
-  messagingSenderId: "781353024788",
-
-  appId: "1:781353024788:web:312c49d154551b8e321601"
+  
 
 }
 
