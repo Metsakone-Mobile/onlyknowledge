2e7cb55a2eeb36b9bdc501c6f0558420a11d3690
Trinkku <trinh.leloan@hotmail.com>
2022-11-14 13:25:58 +0200

    cloudinary access

--- components/Screens/MyProfileScreen/MyProfile.js
+++ components/Screens/MyProfileScreen/MyProfile.js
@@ -11,6 +11,9 @@ import { AntDesign } from '@expo/vector-icons';
 
 export default function MyProfile() {
 
+
+  let CloudinaryUrl = 'https://api.cloudinary.com/v1_1/dapbyrfgw/image/upload';
+
   const { loggedUserID } = useContext(AuthContext)
   const [name, setName] = useState('')
 
@@ -36,8 +39,46 @@ export default function MyProfile() {
 
 
 
-  const pickImage = async() => {
-    let result = await ImagePicker.launchImageLibraryAsync({
+    const openImagePicker = async () => {
+      let permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+        if (permissionResult.granted === false) {
+          Alert.alert('Permission to access camera roll is required!');
+          return;
+    }
+      let pickerResult = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      base64: true
+     });
+        if (!pickerResult.cancelled) {
+          setImage({ image: pickerResult.uri });
+
+          //We need the image to be base64 in order to be formatted for Cloudinary
+          let base64Img = `data:image/jpg;base64,${pickerResult.base64}`;
+          
+          let data = {
+            "file": base64Img,
+            "upload_preset": "wr3ifdpb",
+          }
+  
+    
+  //sends photo to cloudinary
+      fetch(CloudinaryUrl, {
+        body: JSON.stringify(data),
+        headers: {
+          'content-type': 'application/json'
+        },
+        method: 'POST',
+      }).then(async r => {
+          let data = await r.json()
+          console.log(data.secure_url)
+          return data.secure_url
+      }).catch(err=>console.log(err))
+      }
+  }
+/*   const pickImage = async() => {
+    let permissionResultresult = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4,3],
@@ -48,16 +89,16 @@ export default function MyProfile() {
      if(! result.cancelled){
     setImage(result.uri)
      }
-  }
+  } */
   return (
     <View style={myProfileStyles.container}>
       <Text style={myProfileStyles.mainTitle}>My Profile</Text>
 
       <View style={MyProfileStyles.profileCircle}>
-        <Image source={{ uri: image }} style={myProfileStyles.profilePic} />
+        <Image source={{ uri: "https://res.cloudinary.com/dapbyrfgw/image/upload/v1668424808/jbsfvjcd3dkakdavjgfj.png" }} style={myProfileStyles.profilePic} />
     </View>
     <View >
-     <TouchableOpacity onPress={pickImage} > 
+     <TouchableOpacity onPress={openImagePicker} > 
      <AntDesign name="camerao" size={40} color="white" />
       <Text>Change profile picture</Text>
      </TouchableOpacity>
