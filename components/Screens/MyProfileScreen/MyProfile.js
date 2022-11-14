90e2085b299191a30ad7e63f998adeb1359c62e0
Trinkku <trinh.leloan@hotmail.com>
2022-11-14 12:42:36 +0200

    start of uploandimage

--- components/Screens/MyProfileScreen/MyProfile.js
+++ components/Screens/MyProfileScreen/MyProfile.js
@@ -1,15 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity, TextInput, Button} from 'react-native'
 import React, { useContext, useEffect, useState } from 'react'
 import { firestore, doc, getDoc, USER } from '../../../firebase/Config'
 import { AuthContext } from '../../../context/AuthContext'
 import myProfileStyles from './MyProfileStyles'
 import Pickers from '../../Customs/Pickers'
+import * as ImagePicker from 'expo-image-picker'
+import MyProfileStyles from './MyProfileStyles'
+import { AntDesign } from '@expo/vector-icons';
 
 
 export default function MyProfile() {
 
   const { loggedUserID } = useContext(AuthContext)
   const [name, setName] = useState('')
+
+  const [image,setImage]= useState(null)
 
 
   const getUserInfo = async () => {
@@ -29,10 +34,63 @@ export default function MyProfile() {
     getUserInfo()
   }, [])
 
+
+
+  const pickImage = async() => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4,3],
+      quality: 1,
+    })
+    console.log (result);
+    
+     if(! result.cancelled){
+    setImage(result.uri)
+     }
+  }
   return (
     <View style={myProfileStyles.container}>
       <Text style={myProfileStyles.mainTitle}>My Profile</Text>
+
+      <View style={MyProfileStyles.profileCircle}>
+        <Image source={{ uri: image }} style={myProfileStyles.profilePic} />
+    </View>
+    <View >
+     <TouchableOpacity onPress={pickImage} > 
+     <AntDesign name="camerao" size={40} color="white" />
+      <Text>Change profile picture</Text>
+     </TouchableOpacity>
+
+    </View>
+
       <Text style={myProfileStyles.label}>{name}</Text>
+{/* 
+
+<View style={myProfileStyles.placeHolder}>
+    <TextInput style={myProfileStyles.textInput} placeholder='name'/>
+    </View>
+    <View style={myProfileStyles.placeHolder}>
+      <TextInput style={myProfileStyles.textInput} placeholder="Last name"
+  
+      />
+    </View>
+    <View style={myProfileStyles.placeHolder}>
+      <TextInput style={myProfileStyles.textInput} placeholder='email adress'/>
+    </View>
+    <View style={myProfileStyles.placeHolder}>
+      <TextInput style={myProfileStyles.textInput} placeholder='phone number' />
+    </View>
+      <Button title='save'/> 
+    
+
+ */}
+
+
+
+    
+
+
       <Pickers/>
     </View>
   )
--- components/Screens/MyProfileScreen/MyProfileStyles.js
+++ components/Screens/MyProfileScreen/MyProfileStyles.js
@@ -14,4 +14,36 @@ export default StyleSheet.create ({
         fontSize: 24,
         fontWeight: 'bold',
     },
+    profileCircle: {
+        width: 150,
+        height: 150,
+        borderRadius: 150 / 2,
+        backgroundColor: '#FF9800',
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      profilePic:{
+        width: 150,
+        height: 150,
+        borderRadius: 150 / 2,
+      },
+      placeHolder:{
+        flexDirection: 'row',
+        marginTop: 20,
+        marginBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#f2f2f2',
+
+    },
+      textInput:{
+        borderColor: "gray",
+        width: "80%",
+        height: "200%",
+        borderWidth: 1,
+        borderRadius: 10,
+        paddingLeft: 10,
+       
+        
+    
+      }
 })
