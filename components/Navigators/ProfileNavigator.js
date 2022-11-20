b05af2fb23b0b3b51512dc753b64db90da15c268
Trinkku <trinh.leloan@hotmail.com>
2022-11-20 13:41:52 +0200

    updates for profile navigator

--- components/Navigators/ProfileNavigator.js
+++ components/Navigators/ProfileNavigator.js
@@ -1,6 +1,5 @@
 
 import React from 'react'
-import {View, TouchableOpacity, Text} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import MyProfile from '../Screens/MyProfileScreen/MyProfile';
@@ -11,26 +10,10 @@ export default function ProfileNavigator() {
 const Stack = createStackNavigator()
 
 return(
+
     <Stack.Navigator >
-        <Stack.Screen 
-        name='Profile'
-        component={MyProfile}
-        options={{headerShown:false}}
-        />
-        <Stack.Screen
-        name='Edit Profile'
-        component={EditProfile}
-        options={{
-            headerTitle: 'Edit Profile',
-            headerBackTitleVisible: false,
-            headerTitleAlign: 'center',
-            headerStyle: {
-              backgroundColor: '#fff',
-              shadowColor: '#fff',
-              elevation: 0,
-            },
-        }}
-        />
+        <Stack.Screen name=' My Profile' component={MyProfile} />
+        <Stack.Screen name='Edit Profile'component={EditProfile}/>
 
 
     </Stack.Navigator>
