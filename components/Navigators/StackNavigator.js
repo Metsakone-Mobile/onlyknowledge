128d765eda96ae59ec18730b61e23a6aaea35805
JaakkoMal <j.j.malmi@gmail.com>
2022-11-21 20:00:49 +0200

    Quick question functionality started
    User can now state a quick question and check own unanswered questions. Next step is to make to functionality for tutor users to be able to browse and answer quick questions and for users to browse their answered questions.

--- components/Navigators/StackNavigator.js
+++ components/Navigators/StackNavigator.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import HomeScreen from '../Screens/HomeScreen/HomeScreen'
 import FindATutor from '../Screens/FindATutorScreen/FindATutor'
-import QuickQuestion from '../Screens/QuickQuestionScreen/QuickQuestion'
+import QuickQuestionNavigator from './QuickQuestionNavigator'
 
 
 
@@ -15,7 +15,7 @@ export default function StackNavigator() {
     <Stack.Navigator>
         <Stack.Screen name='Home' component={HomeScreen} />
         <Stack.Screen name='Find a tutor' component={FindATutor} />
-        <Stack.Screen name='Quick question' component={QuickQuestion} />
+        <Stack.Screen name='Quick question navigator' component={QuickQuestionNavigator} options={{headerShown: false}} />
     </Stack.Navigator>
   )
 }
---
+++ components/Screens/MyOpenQuestions/MyOpenQuestionsStyles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#e5e5e5',
+        alignItems: 'center',
+    },
+    mainTitle: {
+        marginTop: 50,
+        fontSize: 36,
+        marginBottom: 80
+    }, 
+    questionCard: {
+        flex: 1,
+        width: '80%',
+        backgroundColor: '#f5f2eb',
+        paddingLeft: 20,
+        paddingTop: 5,
+        marginBottom: 20,
+    },
+})
