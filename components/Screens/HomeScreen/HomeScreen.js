91376d38d59537d603ecf0209e81bd618888a79c
JaakkoMal <j.j.malmi@gmail.com>
2022-12-01 21:34:01 +0200

    Simple token payment added
    Users now receive tokens when they create an account.  Stating a quick question deducts 5 tokens and answering a question rewards the user with 3 tokens.

--- components/Screens/HomeScreen/HomeScreen.js
+++ components/Screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,32 @@
-import { View, Text, Pressable } from 'react-native'
-import React from 'react'
+import { View, ScrollView, Pressable } from 'react-native'
+import React, { useState, useContext, useCallback } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
+import { firestore, doc, USER, getDoc } from '../../../firebase/Config'
+import { AuthContext } from '../../../context/AuthContext'
 import homeScreenStyles from './HomeScreenStyles'
 import CustomButton from '../../Customs/Buttons/CustomButton'
 import Title from '../../Customs/TextWrappers/Title'
+import TokenInfo from '../../Customs/TokenInfo'
 
 export default function HomeScreen({ navigation }) {
+
+  const { loggedUserID } = useContext(AuthContext)
+  const [username, setUsername] = useState('')
+  const [tokens, setTokens] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  const getUserInfo = async() => {
+    const docRef = doc(firestore, 'user', loggedUserID)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()) {
+      setUsername(docSnap.data().username)
+      setTokens(docSnap.data().tokens)
+      setIsLoaded(true)
+    } else {
+      console.log(`can't find user with id ${loggedUserID}`)
+    }
+  }
 
   const goToFindATutor = () => {
     navigation.navigate('Find a tutor')
@@ -13,15 +35,32 @@ export default function HomeScreen({ navigation }) {
   const goToQuickQuestion = () => {
     navigation.navigate('Quick question navigator')
   }
+
+  useFocusEffect(
+    useCallback(() => {
+      if(loggedUserID){
+        getUserInfo()
+      }
+    }, [loggedUserID])
+  )
+
   return (
+    <ScrollView style={{backgroundColor: '#e5e5e5'}}>
     <View style={homeScreenStyles.container}>
       <Title text="Only Knowledge" />
-        <Pressable onPress={goToFindATutor} style={{margin: 40}}>
+        <Pressable onPress={goToFindATutor} style={{margin: 30}}>
           {(state) => <CustomButton pressed={state.pressed} buttonText='Find a tutor' />}
         </Pressable>
-        <Pressable onPress={goToQuickQuestion} style={{margin: 20}}>
+        <Pressable onPress={goToQuickQuestion} style={{margin: 15}}>
           {(state) => <CustomButton pressed={state.pressed} buttonText='Quick question' />}
-        </Pressable> 
+        </Pressable>
+        {
+          isLoaded ? 
+          <TokenInfo username={username} tokens={tokens}/>
+          :
+          null
+        } 
     </View>
+    </ScrollView>
   )
 }
